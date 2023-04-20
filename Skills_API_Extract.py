d85fba930776c702fa191e7a38ee05a407df215a
Antpapa99 <anthony.oram@outlook.com>
2023-04-20 12:52:00 +0200

    Skills and their occurences

--- Skills_API_Extract.py
+++ Skills_API_Extract.py
@@ -2,8 +2,6 @@ import requests
 import json
 
 
-jobs_url = "https://jobsearch.api.jobtechdev.se/complete?occupation-field=apaJ_2ja_LuF&limit=50&contextual=true"
-
 
 
 def fetch_data(url):
@@ -17,24 +15,26 @@ def fetch_data(url):
 
 def process_jobs(jobs_data):
     jobs = []
+    occurence = []
     for job_data in jobs_data["typeahead"]:
         job_title = job_data["value"]
+        occurence_title = job_data["occurrences"]
         if job_data["type"] == 'skill':
-            jobs.append(job_title)
+            jobs.append((job_title, occurence_title))
     return jobs
 
 def main():
-        try:
-            jobs_data = fetch_data(jobs_url)
+    jobs_url = f"https://jobsearch.api.jobtechdev.se/complete?occupation-field=apaJ_2ja_LuF&qfields=skill&limit=50"
+    try:
+        jobs_data = fetch_data(jobs_url)
 
-            jobs = process_jobs(jobs_data)
+        jobs = process_jobs(jobs_data)
 
-            print("Jobs:")
-            for i, job in enumerate(jobs):
-                print(f"{i + 1}. {job}")
-
-        except Exception as e:
-            print(f"Error: {e}")
+        print("Jobs:")
+        for i, job in enumerate(jobs):
+            print(f"{i + 1}. {job}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
