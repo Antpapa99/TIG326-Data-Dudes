1591913d01e588f6cb9906998072dbef460e5aab
Antpapa99 <anthony.oram@outlook.com>
2023-04-24 22:39:53 +0200

    Expanded skills list

--- Skills_Selector.py
+++ Skills_Selector.py
@@ -1,7 +1,9 @@
 import json
 def select_skills():
-    with open("Skills.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
+    with open("Skills.json", "r", encoding="utf-8") as file1, open("Stack_Overflow_Skills.json", "r", encoding="utf-8") as file2:
+        data = json.load(file1)
+        data2 = json.load(file2)
+        data.update(data2)
 
         skills_select = [{"label": label, "value": value} for label, value in zip(data["label"], data["value"])]
 
