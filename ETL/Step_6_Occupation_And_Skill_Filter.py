624682882b4be95793b81ba9bb46d84fa20037c3
Antpapa99 <anthony.oram@outlook.com>
2023-05-05 11:51:59 +0200

    Skill whitelist code adjusted to the changes

--- ETL/Step_6_Occupation_And_Skill_Filter.py
+++ ETL/Step_6_Occupation_And_Skill_Filter.py
@@ -14,12 +14,12 @@ def is_valid_skill(skill):
     # Check if the skill is in the whitelist
     if skill in skills_whitelist["label"]:
         return True
-    
+
     # Check if the skill is an abbreviation or misspelling of a valid skill
     for valid_skill in skills_whitelist["label"]:
         if skill.startswith(valid_skill):
             return True
-    
+
     # If the skill isn't valid or an abbreviation/misspelling, return False
     return False
 
@@ -34,7 +34,7 @@ for occupation in occupation_list:
     # Loop through each skill in the occupation's skill list
     for skill in occupation['skills']:
         # Check if the skill is valid
-        if is_valid_skill(skill):
+        if is_valid_skill(skill['name']):
             # If the skill is valid, add it to the list of valid skills
             valid_skills.append(skill)
     # Replace the occupation's skills with the list of valid skills
@@ -44,24 +44,3 @@ for occupation in occupation_list:
 with open('updated_occupation_list.json', 'w') as f:
     json.dump(occupation_list, f, indent=4)
 
-# Create empty lists to hold the labels and values of valid skills
-valid_labels = []
-valid_values = []
-
-# Loop through each occupation in the occupation list
-for occupation in occupation_list:
-    # Loop through each skill in the occupation's skill list
-    for skill in occupation['skills']:
-        # Check if the skill is valid
-        if is_valid_skill(skill):
-            # If the skill is valid, add its label and value to the corresponding lists
-            valid_labels.append(skill)
-
-# Create a dictionary with the valid labels and values
-valid_labels = set(valid_labels)
-valid_labels = list(valid_labels)
-valid_skills_dict = {"label": valid_labels, "value": valid_labels}
-
-# Save the valid skills to a new JSON file
-with open('valid_skills.json', 'w') as f:
-    json.dump(valid_skills_dict, f, indent=4)
