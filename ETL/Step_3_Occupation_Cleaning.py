42e702f982c028aaf9c40e282f4d1369ccf4f6a0
Antpapa99 <anthony.oram@outlook.com>
2023-05-05 11:48:08 +0200

    Added skill and a occurence count

--- ETL/Step_3_Occupation_Cleaning.py
+++ ETL/Step_3_Occupation_Cleaning.py
@@ -9,15 +9,25 @@ for item in data:
     occupation_type = item['Occupation-type']
     if occupation_type not in merged_data:
         merged_data[occupation_type] = {
-            'title': occupation_type,
-            'skills': item['Skills']
+            'label': occupation_type,
+            'skills': []
         }
-    else:
-        merged_data[occupation_type]['skills'].extend(item['Skills'])
+    for skill in item['Skills']:
+        skill_found = False
+        for existing_skill in merged_data[occupation_type]['skills']:
+            if existing_skill['name'] == skill:
+                existing_skill['count'] += 1
+                skill_found = True
+                break
+        if not skill_found:
+            merged_data[occupation_type]['skills'].append({
+                'name': skill,
+                'count': 1
+            })
 
-# Remove duplicates in skills list
-for occupation_type, item in merged_data.items():
-    item['skills'] = list(set(item['skills']))
+# Sort skills based on the count
+for occupation_type in merged_data.values():
+    occupation_type['skills'] = sorted(occupation_type['skills'], key=lambda x: x['count'], reverse=True)
 
 # Convert merged_data into a list of dictionaries
 merged_data_list = list(merged_data.values())
@@ -25,3 +35,5 @@ merged_data_list = list(merged_data.values())
 # Save the merged data to a new JSON file
 with open('merged_jobdictionary.json', 'w') as file:
     json.dump(merged_data_list, file, ensure_ascii=False, indent=4)
+
+print("Done")
