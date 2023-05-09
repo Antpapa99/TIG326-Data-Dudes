f12fa5e84977c7a5258c72522438594849a70d57
Antpapa99 <anthony.oram@outlook.com>
2023-05-09 20:33:08 +0200

    adjusting the changes to the code

--- ETL/Step_3_Occupation_Cleaning.py
+++ ETL/Step_3_Occupation_Cleaning.py
@@ -1,71 +1,39 @@
 import json
 
-with open('job_dictionary.json', 'r') as file:
+with open('Job_Dictionary.json', 'r') as file:
     data = [json.loads(line) for line in file]
 
-# Merge dictionaries with the same occupation-type and AI occupation classify
+# Merge dictionaries with the same occupation-AI_classify
 merged_data = {}
 for item in data:
-    occupation_type = item['Occupation-type']
-    ai_occupations = item['Occupation-AI_classify']
-
-    if occupation_type not in merged_data:
-        merged_data[occupation_type] = {
-            'label': occupation_type,
-            'skills': [],
-            'ai_occupations': []
-        }
-
-    for skill in item['Skills']:
-        skill_found = False
-        for existing_skill in merged_data[occupation_type]['skills']:
-            if existing_skill['name'] == skill:
-                existing_skill['count'] += 1
-                skill_found = True
-                break
-        if not skill_found:
-            merged_data[occupation_type]['skills'].append({
-                'name': skill,
-                'count': 1
-            })
-
-    for ai_occupation in ai_occupations:
-        ai_occ_found = False
-        for existing_ai_occ in merged_data[occupation_type]['ai_occupations']:
-            if existing_ai_occ['name'] == ai_occupation:
-                existing_ai_occ['count'] += 1
-                skill_found = False
-                for existing_skill in existing_ai_occ['skills']:
-                    if existing_skill['name'] == skill:
-                        existing_skill['count'] += 1
-                        skill_found = True
-                        break
-                if not skill_found:
-                    existing_ai_occ['skills'].append({
-                        'name': skill,
-                        'count': 1
-                    })
-                ai_occ_found = True
-                break
-
-        if not ai_occ_found:
-            merged_data[occupation_type]['ai_occupations'].append({
-                'name': ai_occupation,
-                'count': 1,
-                'skills': [{
+    for occupation_ai_classify in item['Occupation-AI_classify']:
+        if occupation_ai_classify not in merged_data:
+            merged_data[occupation_ai_classify] = {
+                'label': occupation_ai_classify,
+                'skills': []
+            }
+        for skill in item['Skills']:
+            skill_found = False
+            for existing_skill in merged_data[occupation_ai_classify]['skills']:
+                if existing_skill['name'] == skill:
+                    existing_skill['count'] += 1
+                    skill_found = True
+                    break
+            if not skill_found:
+                merged_data[occupation_ai_classify]['skills'].append({
                     'name': skill,
                     'count': 1
-                }]
-            })
+                })
 
 # Sort skills based on the count
-for occupation_type in merged_data.values():
-    occupation_type['skills'] = sorted(occupation_type['skills'], key=lambda x: x['count'], reverse=True)
-    for ai_occupation in occupation_type['ai_occupations']:
-        ai_occupation['skills'] = sorted(ai_occupation['skills'], key=lambda x: x['count'], reverse=True)
+for occupation_ai_classify in merged_data.values():
+    occupation_ai_classify['skills'] = sorted(occupation_ai_classify['skills'], key=lambda x: x['count'], reverse=True)
+
+# Convert merged_data into a list of dictionaries
+merged_data_list = list(merged_data.values())
 
 # Save the merged data to a new JSON file
 with open('merged_jobdictionary.json', 'w') as file:
-    json.dump(list(merged_data.values()), file, ensure_ascii=False, indent=4)
+    json.dump(merged_data_list, file, ensure_ascii=False, indent=4)
 
 print("Done")
