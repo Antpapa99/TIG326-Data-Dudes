ac195ee63f2bd0e388a01db9908a03edddc9d5c0
Antpapa99 <anthony.oram@outlook.com>
2023-05-09 22:10:47 +0200

    test

--- Skills_Selector.py
+++ Skills_Selector.py
@@ -1,6 +1,6 @@
 import json
 def select_skills():
-    with open("skills_combined.json", "r", encoding="utf-8") as file1:
+    with open("exact_skills.json", "r", encoding="utf-8") as file1:
         data = json.load(file1)
 
         skills_select = [{"label": label, "value": value} for label, value in zip(data["label"], data["value"])]
