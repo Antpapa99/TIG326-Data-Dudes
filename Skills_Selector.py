475c4b5f9f258728829de52a8dbc2370fc6ae361
Dyrker <elmer.blixt@gmail.com>
2023-05-10 11:33:13 +0200

    Update Skills_Selector.py

--- Skills_Selector.py
+++ Skills_Selector.py
@@ -3,10 +3,15 @@ import json
 def select_skills():
     with open("updated_occupation_list.json", "r", encoding="utf-8") as file1:
         data = json.load(file1)
-    
+
     skills_select = []
     for job in data:
         skills_select.extend([skill['name'] for skill in job['skills']])
-        
+
     exact_skills = list(set(skills_select))
 
+    result = []
+    for skill in exact_skills:
+        result.append({"label": skill, "value": skill})
+
+    return result
