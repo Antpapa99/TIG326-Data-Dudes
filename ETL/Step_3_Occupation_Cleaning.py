691fca79f80be1f27951f4a8804995ce318e039f
Antpapa99 <anthony.oram@outlook.com>
2023-05-11 12:20:41 +0200

    Add occupation count

--- ETL/Step_3_Occupation_Cleaning.py
+++ ETL/Step_3_Occupation_Cleaning.py
@@ -5,13 +5,21 @@ with open('Job_Dictionary.json', 'r') as file:
 
 # Merge dictionaries with the same occupation-AI_classify
 merged_data = {}
+occupation_counts = {}
+
 for item in data:
     for occupation_ai_classify in item['Occupation-AI_classify']:
         if occupation_ai_classify not in merged_data:
             merged_data[occupation_ai_classify] = {
                 'label': occupation_ai_classify,
-                'skills': []
+                'skills': [],
+                'count': 1  # Increment count for each occupation encountered
             }
+            occupation_counts[occupation_ai_classify] = 1  # Initialize occupation count
+        else:
+            merged_data[occupation_ai_classify]['count'] += 1
+            occupation_counts[occupation_ai_classify] += 1
+
         for skill in item['Skills']:
             skill_found = False
             for existing_skill in merged_data[occupation_ai_classify]['skills']:
@@ -35,5 +43,9 @@ merged_data_list = list(merged_data.values())
 # Save the merged data to a new JSON file
 with open('merged_jobdictionary.json', 'w') as file:
     json.dump(merged_data_list, file, ensure_ascii=False, indent=4)
+
+# Print occupation counts
+for occupation, count in occupation_counts.items():
+    print(f"{occupation}: {count}")
 
 print("Done")
