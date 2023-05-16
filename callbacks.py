b5bac824482dbb7936634ce1d8db32f5b34bb1c6
Antpapa99 <anthony.oram@outlook.com>
2023-05-16 13:59:16 +0200

    Added user friendliness

--- callbacks.py
+++ callbacks.py
@@ -50,23 +50,21 @@ def update_graph(n_clicks, selected_job):
         }
     return go.Figure()
 
-#Matching mechanism
 @app.callback(
     dash.dependencies.Output('job-matches', 'children'),
-    [dash.dependencies.Input('submit-button', 'n_clicks')],
-    [dash.dependencies.State('skills-dropdown', 'value'),
-     dash.dependencies.State('selected-job', 'children')]
+    [dash.dependencies.Input('skills-dropdown', 'value'),
+     dash.dependencies.Input('match-skills-button', 'n_clicks'),
+     dash.dependencies.Input('job-link-styles', 'data')]
 )
-def match_jobs(n_clicks, selected_skills, selected_job):
-    if not selected_skills:
+def match_jobs(selected_skills, n_clicks, styles):
+    if not n_clicks or not selected_skills:
         return "No matches"
     matches = []
     for job in new_Jobs:
         required_skills = set(skill['name'] for skill in job['skills'])
         seeker_skills = set(selected_skills)
-        if required_skills and seeker_skills.issubset(required_skills):
-            # Check if the current job is the selected job
-            button_style = {'background-color': 'green'} if job['label'] == selected_job else {}
+        if required_skills and (seeker_skills & required_skills): 
+            button_style = styles.get(job['label'], {})
             matches.append(html.Div([
                 html.Button(job['label'], id={'type': 'job-link', 'index': job['label']}, style=button_style),
             ]))
@@ -81,9 +79,11 @@ def match_jobs(n_clicks, selected_skills, selected_job):
 @app.callback(
     dash.dependencies.Output('job-skills', 'children'),
     [dash.dependencies.Input('jobs-dropdown', 'value'),
-     dash.dependencies.Input('hidden-div', 'children')]
+     dash.dependencies.Input('hidden-div', 'children'),
+     dash.dependencies.Input('job-link-styles', 'data')],  # Add this line
+    [dash.dependencies.State('skills-dropdown', 'value')]
 )
-def display_job_skills(dropdown_value, clicked_job,):
+def display_job_skills(dropdown_value, clicked_job, styles, selected_skills):
     ctx = dash.callback_context
     if not ctx.triggered:
         return "No job selected"
@@ -92,6 +92,10 @@ def display_job_skills(dropdown_value, clicked_job,):
         # If the trigger comes from the dropdown
         if trigger_id == 'jobs-dropdown':
             selected_job_label = dropdown_value
+        elif trigger_id == 'job-link-styles':  # Add this condition
+            selected_job_label = next(
+                job for job, style in styles.items() if style.get('background-color') == 'green'
+            )
         else:  # trigger_id == 'hidden-div'
             selected_job_label = clicked_job
     if selected_job_label:
@@ -101,20 +105,49 @@ def display_job_skills(dropdown_value, clicked_job,):
             return html.Ul([
                 html.Li(
                     f"{skill['name']} ({skill['count']})",
-                    style={"font-weight": "bold", "color": "red"} if skill['name'] == "python" else {}
+                    style={"font-weight": "bold", "color": "red"} if skill['name'] in selected_skills else {}
                 )
                 for skill in skills
             ])
     return "No job selected"
 
+
+def highlight_selected_job(dropdown_value, clicked_job, ids):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return [{} for _ in ids]
+    else:
+        trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
+        # If the trigger comes from the dropdown
+        if trigger_id == 'jobs-dropdown':
+            selected_job_label = dropdown_value
+        else:  # trigger_id == 'hidden-div'
+            selected_job_label = clicked_job
+
+    return [{'background-color': 'green'} if id['index'] == selected_job_label else {} for id in ids]
+
 @app.callback(
-    dash.dependencies.Output('hidden-div', 'children'),
+    dash.dependencies.Output('job-link-styles', 'data'),
     [dash.dependencies.Input({'type': 'job-link', 'index': dash.dependencies.ALL}, 'n_clicks')],
-    [dash.dependencies.State({'type': 'job-link', 'index': dash.dependencies.ALL}, 'id')]
+    [dash.dependencies.State({'type': 'job-link', 'index': dash.dependencies.ALL}, 'id'),
+     dash.dependencies.State('job-link-styles', 'data')]
 )
-def handle_job_click(n_clicks, ids):
+def handle_job_click(n_clicks, ids, styles):
     if not any(n_clicks):
-        return dash.no_update
+        return styles  # No buttons have been clicked yet, return current styles
     # Get the label of the clicked job
     clicked_job_label = next(id['index'] for n_click, id in zip(n_clicks, ids) if n_click)
-    return clicked_job_label
+    # Reset all styles and set the style of the clicked job to green
+    new_styles = {id['index']: {} for id in ids}  # reset styles
+    new_styles[clicked_job_label] = {'background-color': 'green'}  # highlight clicked job
+    return new_styles
+
+
+@app.callback(
+    [dash.dependencies.Output({'type': 'job-link', 'index': dash.dependencies.ALL}, 'style')],
+    [dash.dependencies.Input('job-link-styles', 'data')],
+    [dash.dependencies.State({'type': 'job-link', 'index': dash.dependencies.ALL}, 'id')]
+)
+
+def update_job_link_styles(styles, ids):
+    return [styles.get(id['index'], {}) for id in ids]
