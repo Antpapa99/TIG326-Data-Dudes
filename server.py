47319b1cfa9faadf43d73576d42922bfbf57c1ae
Antpapa99 <anthony.oram@outlook.com>
2023-05-17 14:51:17 +0200

    Revised the frontpage

--- server.py
+++ server.py
@@ -6,8 +6,9 @@ server = Flask(__name__, static_folder='static/style')
 
 @server.route('/', methods=['GET', 'POST'])
 def home():
-    image_path = url_for('static', filename='img/skillflair.PNG')
-    return render_template('home.html', image_path=image_path)
+    image_path1 = url_for('static', filename='img/skillflair.PNG')
+    image_path2 = url_for('static', filename='img/skillflair_frontpage.PNG')
+    return render_template('home.html', image_path1=image_path1, image_path2=image_path2)
 
 app = dash.Dash(__name__, server=server, routes_pathname_prefix="/dash/",
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
